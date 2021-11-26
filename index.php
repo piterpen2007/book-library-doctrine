ba29ff17f2a4b950e944628211a579b8c55d9e85
fedyancev.d.a <fedyancev.d.a@effective-group.ru>
2021-11-26 13:08:58 +0300

    Загрузка данных из файла

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
+$pathToAuthors = __DIR__ . '/authors.json';
+$authorsTxt = file_get_contents($pathToAuthors);
+$authorsJson = json_decode($authorsTxt,true);
+
+$pathToBooks = __DIR__ . '/books.json';
+$booksTxt = file_get_contents($pathToBooks);
+$booksJson = json_decode($booksTxt,true);
+
 if('/books' === $_SERVER['PATH_INFO']) {
     $httpCode = 200;
-    $result = [
-        [
-            'id' => 10,
-            'title' => 'Мечтают ли андроиды об электроовцах?',
-            'year' => 1966,
-            'author_id' => 5
-        ]
-    ];
+    $result = [];
+    foreach ($booksJson as $book) {
+        if (array_key_exists('title', $_GET) && $book['title'] === $_GET['title']) {
+            $result[] = $book;
+        }
+    }
 } elseif ('/authors' === $_SERVER['PATH_INFO']) {
     $httpCode = 200;
-    $result = [
-        [
-            'id' => 1,
-            'name' => 'Чак',
-            'surname' => 'Паланик',
-            'birthday' => '21.02.1962',
-        ]
-    ];
+    $result = [];
+    foreach ($authorsJson as $author) {
+        if (array_key_exists('surname',$_GET) && $author['surname'] === $_GET['surname']) {
+            $result[] = $author;
+        }
+    }
 } else {
     $httpCode = 404;
     $result = [
