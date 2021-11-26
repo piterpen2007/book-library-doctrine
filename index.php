72152604f43016814bfd83088e69ced1d0395d48
fedyancev.d.a <fedyancev.d.a@effective-group.ru>
2021-11-26 12:38:53 +0300

    Ход урока

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
-echo 'Hello';
+if('/books' === $_SERVER['PATH_INFO']) {
+    $httpCode = 200;
+    $result = [
+        [
+            'id' => 10,
+            'title' => 'Мечтают ли андроиды об электроовцах?',
+            'year' => 1966,
+            'author_id' => 5
+        ]
+    ];
+} elseif ('/authors' === $_SERVER['PATH_INFO']) {
+    $httpCode = 200;
+    $result = [
+        [
+            'id' => 1,
+            'name' => 'Чак',
+            'surname' => 'Паланик',
+            'birthday' => '21.02.1962',
+        ]
+    ];
+} else {
+    $httpCode = 404;
+    $result = [
+        [
+            'status' => 'fail',
+            'message' => 'unsupported request'
+        ]
+    ];
+}
+header('Content-Type: application/json');
+http_response_code($httpCode);
+echo json_encode($result);
