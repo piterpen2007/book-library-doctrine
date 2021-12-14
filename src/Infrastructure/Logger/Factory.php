df19a25914b1520790b873e101d92b33a0ea54fb
fedyancev.d.a <fedyancev.d.a@effective-group.ru>
2021-12-14 10:00:35 +0300

    Доработка namespace

--- src/Infrastructure/Logger/Factory.php
+++ src/Infrastructure/Logger/Factory.php
@@ -2,6 +2,9 @@
 namespace EfTech\BookLibrary\Infrastructure\Logger;
 use Exception;
 use EfTech\BookLibrary\Infrastructure\AppConfig;
+use EfTech\BookLibrary\Infrastructure\Logger\FileLogger as FileLogger;
+use EfTech\BookLibrary\Infrastructure\Logger\NullLogger as NullLogger;
+use EfTech\BookLibrary\Infrastructure\Logger\EchoLogger as EchoLogger;
 require_once __DIR__ . '/LoggerInterface.php';
 require_once __DIR__ . '/../AppConfig.php';
 
@@ -20,13 +23,13 @@ class Factory
     {
         if ('fileLogger' === $appConfig->getLoggerType()) {
             require_once __DIR__ . '/FileLogger/Logger.php';
-            $logger = new \EfTech\BookLibrary\Infrastructure\Logger\FileLogger\Logger($appConfig->getPathToLogFile());
+            $logger = new FileLogger\Logger($appConfig->getPathToLogFile());
         } elseif ('nullLogger' === $appConfig->getLoggerType()) {
             require_once __DIR__ . '/NullLogger/Logger.php';
-            $logger = new \EfTech\BookLibrary\Infrastructure\Logger\NullLogger\Logger();
+            $logger = new NullLogger\Logger();
         } elseif ('echoLogger' === $appConfig->getLoggerType()) {
             require_once __DIR__ . '/EchoLogger/Logger.php';
-            $logger = new \EfTech\BookLibrary\Infrastructure\Logger\EchoLogger\Logger();
+            $logger = new EchoLogger\Logger();
         } else {
             throw new Exception('Unknown logger type');
         }
